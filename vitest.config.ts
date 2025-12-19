1695eca37f9d8e78f3470f699fc7efdb857b2951
Samuel Huber <samuel@dtech.vision>
2025-12-19 04:27:19 +0100

    fix:  ignore worktree/**

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
   test: {
     include: ["./test/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    exclude: [],
+    exclude: ["./worktree/**", "**/node_modules/**"],
     globals: true,
     coverage: {
       provider: "v8"
